075cc7231694cd2849dbf6e2a5d918969bb51a7c
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2023-12-01 18:01:06 +0300

    Delete umutxyp-slash-and-prefix-bot directory

--- umutxyp-slash-and-prefix-bot/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    token: "", //Write your bot token here
-    prefix: "!", //Write your normal bot prefix here
-    botStatus: "Umutxyp ♥ Kadrxy" //Write your bot status here
-
-}
-
--- umutxyp-slash-and-prefix-bot/run.bat
+++
@@ -1,7 +0,0 @@
-echo off
-color e
-cls
-:a
-node index.js
-goto a
-pause
